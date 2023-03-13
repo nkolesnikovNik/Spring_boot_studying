70e30441e9e8dc99d06247115d5c3e4e95a3ba8a
Nikolas797 <nkolesnikov@amazinghiring.com>
2023-03-13 19:33:44 +0400

    Add Spring

--- src/main/java/com/ah/EntryPoint.java
+++ src/main/java/com/ah/EntryPoint.java
@@ -1,21 +1,12 @@
 package com.ah;
 
-import io.javalin.Javalin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLException;
-
+@SpringBootApplication
 public class EntryPoint {
 
-    public EntryPoint() throws SQLException {
-    }
-
-    public static void main(String[] args) throws SQLException {
-        Controller controller = new Controller();
-        Javalin app = Javalin.create(/*config*/)
-                //First request
-                .get("/hashUrl", ctx -> ctx.result(controller.saveUrl(ctx)))
-                //Second request
-                .get("/takeUrl", ctx -> ctx.result(controller.getUrl(ctx)))
-                .start(7070);
+    public static void main(String[] args) {
+        SpringApplication.run(EntryPoint.class);
     }
 }
--- src/main/java/com/ah/Hasher.java
+++ src/main/java/com/ah/Hasher.java
@@ -6,7 +6,7 @@ import java.util.Base64;
 public class Hasher {
 
     //Generate hash from URL
-    public String getHash(String url) throws Exception {
+    public static String getHash(String url) throws Exception {
         MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] arr = md.digest(url.getBytes());
         return Base64.getEncoder().encodeToString(arr);
